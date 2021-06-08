3ba9716347993488e709fc9b7dc1eb5487d5247e
FelipeIotti <felipeizago@hotmail.com>
2021-06-08 20:04:44 -0300

    atualizacao de imagem

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,7 +7,7 @@ export function Profile() {
 
     return(
         <div className={styles.profileContainer} >
-            <img src="https://scontent.fgru6-1.fna.fbcdn.net/v/t1.0-9/75552857_2484983544903262_2152073340246818816_n.jpg?_nc_cat=106&ccb=3&_nc_sid=09cbfe&_nc_ohc=qWdUM8tlokMAX_k8FZj&_nc_ht=scontent.fgru6-1.fna&oh=a945afe2ebfc1e06402bff6e6f87a721&oe=605E1281" alt="Iotti"/>
+            <img src="https://scontent.fbau1-1.fna.fbcdn.net/v/t1.6435-9/75552857_2484983544903262_2152073340246818816_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=rWQ0zrxQTeoAX8GJWgC&_nc_ht=scontent.fbau1-1.fna&oh=8b9c81c7c000d28a3798d5b9afe10339&oe=60E51AD3"/>
             <div>
                 <strong>Felipe Iotti</strong>
                 <p>
